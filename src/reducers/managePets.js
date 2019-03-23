89961fef3802186ced375a383884e04aed645ad4
Felice Forby <feliceforby@gmail.com>
2019-03-23 03:03:36 +0000

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,40 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = {pets: []}, action) {
+  switch(action.type) {
+    case "ADD_PET":
+      return {
+       pets: [...state.pets, action.pet]
+      };
+    case "REMOVE_PET":
+      let removalIndex = state.pets.findIndex(pet => pet.id === action.id);
+      return {
+        pets: [
+          ...state.pets.slice(0, removalIndex),
+          ...state.pets.slice(removalIndex + 1)
+        ]
+      }
+    default:
+      return state;
+  }
 }
 
-export function dispatch(){
+export function dispatch(action) {
+  state = managePets(state, action);
+  render();
 }
 
-export function render(){
+export function render() {
+  const container = document.getElementById('container');
+  
+  if (state.pets.length > 0) {
+    container.innerHTML = '<ul></ul>';
+    const ul = document.querySelector('ul');
+    
+    state.pets.forEach(pet => {
+      ul.innerHTML += `<li>${pet.name}</li>`;
+    });
+  }
 
 }
