cf2eff1d9b70260841aadd3b5a02eba8f6bd4d23
Felice Forby <feliceforby@gmail.com>
2019-03-23 02:32:14 +0000

    Automatically backed up by Learn

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -5,7 +5,7 @@ export function managePets(state = {pets: []}, action) {
   switch(action.type) {
     case "ADD_PET":
       return state;
-    case "REMOVE_PET"
+    case "REMOVE_PET":
       return state;
     default:
       return state;
